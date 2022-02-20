6a6579d33172729b69da26cc3d57436af65629c5
Sim <https://github.com/VladiNaumov>
2022-02-20 12:35:40 +0200

    Ml

--- training/app/my_ex_02.py
+++ training/app/my_ex_02.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 # импортируем модуль, отвечающий за линейную регрессию
 from sklearn.linear_model import LinearRegression
 
-""" houses = pd.read_csv('C:\\Users\\Sim\\Desktop\\MLTrainings\\training\\data\\houses.csv') """
+""" phones = pd.read_csv('C:/Users/Sim/jupiter-book/data/phones.csv', sep=',') """
 
 phones = pd.read_csv('../../data/phones.csv', sep=',')
 phones_info = phones.head(5)
@@ -25,10 +25,9 @@ ax.scatter(phones["disk"], phones["price"], s=100)
 # отображаем картинку
 plt.show()
 
-"""
 # выгружаем признаки и целевые значения в отдельные переменные
-X = phones[["_"]]
-y = phones["_"]
+X = phones[["price"]]
+y = phones["disk"]
 
 # создаем регрессор
 reg = LinearRegression().fit(X, y)
@@ -38,24 +37,20 @@ reg = LinearRegression().fit(X, y)
 a = reg.intercept_
 
 # функцию для предсказания цены дома
-def reg_prediction(dim_1):
-    return a + b * dim_1
+def reg_prediction(number):
+    return a + b * number
 
-reg_prediction(X.dim_1[0])
-
-# используем встроенные методы для расчета предсказаний
-# reg.predict(X[0:1])[0]
+inf = reg_prediction(X.price[0])
+print(inf)
 
 # создаем картинку
 fig = plt.figure(figsize=(10, 10))
 ax = plt.axes()
 
 # помещаем точки на график
-ax.scatter(phones["_"], phones["_"], s=100)
+ax.scatter(phones["price"], phones["disk"], s=100)
 # помещаем предсказания
-ax.plot([X.dim_1.min(), X.dim_1.max()], [reg_prediction(X.dim_1.min()), reg_prediction(X.dim_1.max())], c="red")
+ax.plot([X.price.min(), X.price.max()], [reg_prediction(X.price.min()), reg_prediction(X.price.max())], c="red")
 
 # отображаем картинку
-
-"""
 plt.show()
